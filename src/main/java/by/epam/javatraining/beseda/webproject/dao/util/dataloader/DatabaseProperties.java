7006c2909d2805d275b953387c6d514f91020e1b
Maryia Biaseda <maryia_biaseda@epam.com>
2019-09-19 13:09:50 +0300

    some bugs fixed

--- src/main/java/by/epam/javatraining/beseda/webproject/dao/util/dataloader/DatabaseProperties.java
+++ src/main/java/by/epam/javatraining/beseda/webproject/dao/util/dataloader/DatabaseProperties.java
@@ -1,20 +1,14 @@
 package by.epam.javatraining.beseda.webproject.dao.util.dataloader;
 
-import org.apache.log4j.Logger;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
+import java.util.ResourceBundle;
 
-import static by.epam.javatraining.beseda.webproject.util.LoggerName.ERROR_LOGGER;
-import static by.epam.javatraining.beseda.webproject.util.resourceloader.GeneralProperties.PROJECT_LOCATION;
 
 public class DatabaseProperties {
 
-	private static final Logger log = Logger.getLogger(ERROR_LOGGER);
 
-	private static final String DATABASE_PROP_LOCATION;
-	private static final Properties DATABASE_PROP;
+	private static final String DATABASE_PROP_NAME;
+	private static final ResourceBundle DATABASE_PROP;
 
 	public static final String DATABASE_URL;
 
@@ -30,23 +24,13 @@ public class DatabaseProperties {
 	}
 
 	static {
-		DATABASE_PROP_LOCATION = PROJECT_LOCATION + "src/main/resources/database.properties";
+		DATABASE_PROP_NAME = "database";
 		UNICODE_PATCH = "?useUnicode=true&characterEncoding=utf-8";
 		TIMEZONE_PATCH = "&useLegacyDatetimeCode=false&serverTimezone=UTC";
-
-		DATABASE_PROP = new Properties();
-		loadProperties();
-		DATABASE_URL = DATABASE_PROP.getProperty("DATABASE_URL").trim() + UNICODE_PATCH + TIMEZONE_PATCH;
-		DATABASE_USER = DATABASE_PROP.getProperty("DATABASE_USER").trim();
-		DATABASE_PASSWORD = DATABASE_PROP.getProperty("DATABASE_PASSWORD").trim();
-		DATABASE_ENCODING = DATABASE_PROP.getProperty("DATABASE_ENCODING").trim();
-	}
-
-	private static void loadProperties() {
-		try {
-			DATABASE_PROP.load(new FileInputStream(DATABASE_PROP_LOCATION));
-		} catch (IOException ex) {
-			log.error("Error loading database properties file: " + ex);
-		}
+		DATABASE_PROP = ResourceBundle.getBundle(DATABASE_PROP_NAME);
+		DATABASE_URL = DATABASE_PROP.getString("DATABASE_URL").trim() + UNICODE_PATCH + TIMEZONE_PATCH;
+		DATABASE_USER = DATABASE_PROP.getString("DATABASE_USER").trim();
+		DATABASE_PASSWORD = DATABASE_PROP.getString("DATABASE_PASSWORD").trim();
+		DATABASE_ENCODING = DATABASE_PROP.getString("DATABASE_ENCODING").trim();
 	}
 }
--- src/main/java/by/epam/javatraining/beseda/webproject/util/resourceloader/GeneralProperties.java
+++ src/main/java/by/epam/javatraining/beseda/webproject/util/resourceloader/GeneralProperties.java
@@ -1,12 +1,6 @@
 package by.epam.javatraining.beseda.webproject.util.resourceloader;
 
-import org.apache.log4j.Logger;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
-import static by.epam.javatraining.beseda.webproject.util.LoggerName.ERROR_LOGGER;
+import java.util.ResourceBundle;
 
 /**
  * Class designed for setting the base project properties.
@@ -16,11 +10,9 @@ import static by.epam.javatraining.beseda.webproject.util.LoggerName.ERROR_LOGGE
  */
 public class GeneralProperties {
 
-	private static Logger log = Logger.getLogger(ERROR_LOGGER);
 
-	private static final Properties GENERAL_PROP;
-	public static final String PROJECT_LOCATION;
-	public static final String GENERAL_PROP_LOCATION;
+	private static final ResourceBundle GENERAL_PROP;
+	public static final String GENERAL_PROP_NAME;
 
 	// Connection pool properties
 	public static final int INITIAL_POOL_SIZE;
@@ -31,24 +23,14 @@ public class GeneralProperties {
 	public static final String SERVER_ENCODING;
 
 	static {
-		PROJECT_LOCATION = "C:/Users/Maryia_Biaseda/git/New_folder/Trucking_company/";
+		GENERAL_PROP_NAME = "general";
+		
+		GENERAL_PROP =  ResourceBundle.getBundle(GENERAL_PROP_NAME);
+		INITIAL_POOL_SIZE = Integer.parseInt(GENERAL_PROP.getString("INITIAL_POOL_SIZE").trim());
+		WAIT_FOR_DB_RESPONSE = Integer.parseInt(GENERAL_PROP.getString("WAIT_FOR_DB_RESPONSE").trim());
 
-		GENERAL_PROP_LOCATION = "src/main/resources/general.properties";
-		GENERAL_PROP = new Properties();
-		loadProperties();
-		INITIAL_POOL_SIZE = Integer.parseInt(GENERAL_PROP.getProperty("INITIAL_POOL_SIZE").trim());
-		WAIT_FOR_DB_RESPONSE = Integer.parseInt(GENERAL_PROP.getProperty("WAIT_FOR_DB_RESPONSE").trim());
-
-		PROJECT_ENCODING = GENERAL_PROP.getProperty("PROJECT_ENCODING");
-		SERVER_ENCODING = GENERAL_PROP.getProperty("SERVER_ENCODING");
+		PROJECT_ENCODING = GENERAL_PROP.getString("PROJECT_ENCODING");
+		SERVER_ENCODING = GENERAL_PROP.getString("SERVER_ENCODING");
 	}
 
-	private static void loadProperties() {
-		try {
-			GENERAL_PROP.load(new FileInputStream(PROJECT_LOCATION + GENERAL_PROP_LOCATION));
-		} catch (IOException ex) {
-			log.error("Error loading general properties file:" + ex);
-		}
-
-	}
 }
